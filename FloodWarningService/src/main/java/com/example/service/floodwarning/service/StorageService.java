c884b761eb3188ec0ac4795c1ee7d339b16ef643
smitchell1 <steve@platformconsulting.io>
2019-10-15 14:03:15 -0500

    Restored StorageService

---
+++ FloodWarningService/src/main/java/com/example/service/floodwarning/service/StorageService.java
@@ -0,0 +1,68 @@
+package com.example.service.floodwarning.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.OperationContext;
+import com.microsoft.azure.storage.blob.BlobContainerPublicAccessType;
+import com.microsoft.azure.storage.blob.BlobRequestOptions;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import javax.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class StorageService {
+    private final String containerName = "images";
+    private final CloudStorageAccount cloudStorageAccount;
+    private final CloudBlobContainer files;
+
+    @Autowired
+    public StorageService(CloudStorageAccount csa) {
+        this.cloudStorageAccount = csa;
+        files = getOrCreateContainerReference(containerName);
+    }
+    @PostConstruct
+    public void postConstruct() {
+        Assert.notNull(cloudStorageAccount, "cloudStorageAccount must not be null");
+        Assert.notNull(files, "files must not be null");
+    }
+
+    private CloudBlobContainer getOrCreateContainerReference(String containerName) {
+        CloudBlobContainer container = null;
+        try {
+            container = this.cloudStorageAccount
+                    .createCloudBlobClient()
+                    .getContainerReference(containerName);
+            container.createIfNotExists(BlobContainerPublicAccessType.CONTAINER, new BlobRequestOptions(), new OperationContext());
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+        return container;
+    }
+
+    public String writeBlobFile(String stationId, byte[] bytes, String extension) throws Exception {
+        CloudBlockBlob blockBlobReference = files.getBlockBlobReference(generateBlobName(stationId, new Date(), extension));
+        try (InputStream in = new ByteArrayInputStream(bytes)) {
+            blockBlobReference.upload(in, bytes.length);
+            ObjectMapper mapper = new ObjectMapper();
+            log.info(mapper.writeValueAsString(blockBlobReference));
+            return blockBlobReference.getStorageUri().getPrimaryUri().toString();
+        }
+    }
+
+    public String generateBlobName(String stationId, Date date, String extension) {
+        return stationId
+                .concat("/")
+                .concat(UUID.randomUUID().toString())
+                .concat(".").concat(extension);
+    }
+}
